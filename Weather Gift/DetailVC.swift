64b589e526df23f06a6e561b323d7c8156a73c14
Amanda Gilmour <amandagilmour@amandas-macbook-air.local>
2017-03-15 11:59:29 -0400

    7.4

---
+++ Weather Gift/DetailVC.swift
@@ -0,0 +1,31 @@
+//
+//  DetailVC.swift
+//  Weather Gift
+//
+//  Created by Amanda Gilmour on 3/14/17.
+//  Copyright © 2017 Amanda Gilmour. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationLabel.text = locationsArray[currentPage]
+       
+    }
+    
+
+
+}
---
+++ Weather Gift/ListVC.swift
@@ -0,0 +1,23 @@
+//
+//  ListVC.swift
+//  Weather Gift
+//
+//  Created by Amanda Gilmour on 3/14/17.
+//  Copyright © 2017 Amanda Gilmour. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+  
+
+}
