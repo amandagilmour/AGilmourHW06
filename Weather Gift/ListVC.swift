f0c2d32f4f7a35a972fb711dcda308e5010099ff
Amanda Gilmour <amandagilmour@amandas-macbook-air.local>
2017-03-19 17:16:10 -0400

    7.7

--- Weather Gift/ListVC.swift
+++ Weather Gift/ListVC.swift
@@ -11,6 +11,9 @@ import UIKit
 class ListVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
     var locationsArray = [String]()
     var currentPage = 0
     override func viewDidLoad() {
@@ -19,6 +22,30 @@ class ListVC: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self 
         tableView.reloadData()
+    }
+    
+    // MARK: - Segues
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToPageVC" {
+            
+            // Identify the table cell (row) that the user tapped, this is passed back to PageVC as currentPage so that PageVC knows which page to create and display
+            currentPage = (tableView.indexPathForSelectedRow?.row)!
+            let controller = segue.destination as! PageVC
+            controller.currentPage = currentPage
+            controller.locationsArray = locationsArray
+        }
+    }
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        if tableView.isEditing == true {
+            tableView.setEditing(false, animated: true)
+            editBarButton.title = "Edit"
+            addBarButton.isEnabled = true
+        } else {
+            tableView.setEditing(true, animated: true)
+            editBarButton.title = "Done"
+            addBarButton.isEnabled = false
+        }
     }
 
 }
@@ -33,7 +60,39 @@ extension ListVC: UITableViewDataSource, UITableViewDelegate {
         return cell 
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        currentPage = indexPath.row
-        print("Hey I've been clicked at indexpath.row \(indexPath.row)")
+        // We don't need to put any code here b/c the cell triggers a segue
+    }
+    //MARK: - TableView Editing Functions
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            locationsArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // First make a copy of the item that you are going to move.
+        let itemToMove = locationsArray[sourceIndexPath.row]
+        // Delete item from the original location (pre-move)
+        locationsArray.remove(at: sourceIndexPath.row)
+        //Insert item into the "to", post-move, location
+        locationsArray.insert(itemToMove, at: destinationIndexPath.row)
+    }
+    // MARK: - TableView code to freeze the first cell. No deleting or moving.
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return(indexPath.row == 0 ? false : true)
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return(indexPath.row == 0 ? false : true)
+    }
+    
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        if proposedDestinationIndexPath.row == 0 {
+            return sourceIndexPath
+        } else {
+            return proposedDestinationIndexPath
+        }
     }
 }
