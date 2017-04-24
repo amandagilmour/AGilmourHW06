99d06b379ab7b8040e4636b1a6b63c77da3727f9
Amanda Gilmour <amandagilmour@Amandas-MacBook-Air.local>
2017-04-24 12:03:00 -0400

    finished

--- Weather Gift/PageVC.swift
+++ Weather Gift/PageVC.swift
@@ -45,8 +45,9 @@ class PageVC: UIPageViewController {
         
         setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
         
-        configurePageControl()
         configureButtons2()
+        configurePageControl()
+        
     }
 
     // MARK: - UI Configuration Method
